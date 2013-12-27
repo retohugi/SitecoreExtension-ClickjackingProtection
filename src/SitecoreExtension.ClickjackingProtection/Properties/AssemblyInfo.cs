4939546a2fca311d05058302821dfbc6262666e2
Reto Hugi <reto.hugi@unic.com>
2013-12-27 23:38:36 +0100

    new guid for project

--- src/SitecoreExtension.ClickjackingProtection/Properties/AssemblyInfo.cs
+++ src/SitecoreExtension.ClickjackingProtection/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SitecoreExtension.sClickjackingProtection")]
+[assembly: AssemblyTitle("SitecoreExtension.ClickjackingProtection")]
 [assembly: AssemblyDescription("A Sitecore Extension")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
