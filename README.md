72ee7b2988f1b99b01a27bd70139f057bd5bc8c5
Reto Hugi <reto.hugi@unic.com>
2013-12-27 22:58:49 +0100

    initial release

---
+++ README.md
@@ -0,0 +1,28 @@
+# Clickjack Protection - a Sitecore Extension
+
+This extension provides basic protection against Clickjacking Attacks (see [Wikipedia](http://en.wikipedia.org/wiki/Clickjacking) for details on what a Clickjack attack is and [OWASP.org](https://www.owasp.org/index.php/Clickjacking_Defense_Cheat_Sheet) for details on different methods of protection against it).
+
+# Features
+## Implemented ##
+- Sitecore instance default setting for X-Frame-Option Header
+- Default setting support for DENY, SAMEORIGIN or empty (X-Frame-Option header)
+
+## Planned ##
+- Site-based X-Frame-Option settings
+- Page-based X-Frame-Option settings through data template that can be inherited from on pages.
+- support for JavaScript based protection (frame breaker)
+- automatic browser / feature detection (and fallbacks)
+- support for ALLOW-FROM values
+
+## Tested on
+* Sitecore 7.0 (but should work with 6.x)
+
+## Installation 
+Install via NuGet Gallery
+<pre>
+  PM> tbd
+</pre>
+
+## Build
+See [Readme.md in the /build folder](https://tbd).
+
---
+++ src/SitecoreExtension.ClickjackingProtection/Pipelines/httpRequestBegin/ClickjackingProtector.cs
@@ -0,0 +1,69 @@
+﻿namespace SitecoreExtension.ClickjackingProtection.Pipelines.httpRequestBegin
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Sitecore.Common;
+    using Sitecore.Pipelines.HttpRequest;
+    using Sitecore.StringExtensions;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ClickjackingProtector : HttpRequestProcessor
+    {
+        /// <summary>
+        /// Gets the header values.
+        /// </summary>
+        /// <value>
+        /// The header values.
+        /// </value>
+        public List<string> HeaderValues
+        {
+            get
+            {
+                return new List<string> { "DENY", "SAMEORIGIN", string.Empty };
+            }
+        }
+
+        /// <summary>
+        /// Gets the header key.
+        /// </summary>
+        /// <value>
+        /// The header key.
+        /// </value>
+        public string HeaderKey
+        {
+            get
+            {
+                return "X-FRAME-OPTIONS";
+            }
+        }
+
+        /// <summary>
+        /// Processes the specified arguments.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        public override void Process(HttpRequestArgs args)
+        {
+            
+            var defaultValue = Sitecore.Configuration.Settings.GetSetting("ClickjackingProtection.DefaultXFrameValue", string.Empty);
+
+            var value = this.HeaderValues.First(v => v.Contains(defaultValue));
+
+            if (value == string.Empty)
+            {
+                return;
+            }
+
+            // remove any already set headers; duplicate headers are a big no no.
+            while (args.Context.Response.Headers.Get(this.HeaderKey) != null)
+            {
+                args.Context.Response.Headers.Remove(this.HeaderKey);
+            }
+
+            args.Context.Response.AddHeader(this.HeaderKey, value);
+        }
+    }
+}
